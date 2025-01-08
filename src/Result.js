8a39be153c57e94ead44cdc78e1867e0ab61398a
usha1116 <ushasahu1116@gmail.com>
2025-01-08 23:09:47 +0530

    first commit

---
+++ src/Result.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Result = ({word, phonetics, meanings, setText}) => {
+  return (
+    <ul>
+      <li className='world'>
+        <h2> {word} </h2>
+        {
+          phonetics.map((phonetic, index) => (
+            <span key={index}> {phonetic.text} </span>
+          ))
+        }
+      </li>
+      {
+        meanings.map((meaning, index) => (
+          <li className='contain' >
+            <h3>Noun</h3>
+
+            <div className='details meaning'>
+              <h3>Meaning</h3>
+              {
+                meaning.definitions.map((definition, index) => (
+                  <p key={index}>- {definition.definition}</p>
+                ))
+              }
+              <p> - "Hello" or equivalent greeting. </p>
+            </div>
+            {
+              meaning.synonyms.length !== 0 &&
+              <div className='details synonyms'>
+                <h3>Synonyms</h3>
+
+                {
+                  meaning.synonyms.map((synonym, index) => (
+                    <span key={index} onClick={()=>setText(synonym)}>
+                       {`${synonym}`}, </span>
+                  ))
+                }
+              </div>
+            }
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+export default Result
